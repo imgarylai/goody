ef2007314326c2b35b35bd5dcfc87be344d42418
Gary Lai <garylai1990@gmail.com>
2012-11-07 22:07:38 +0800

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :quantity, :title
+  validates :description, :image_url, :price, :quantity, :title, presence: true
+  validates :price, :quantity, numericality: {greater_than_or_equal_to: 0}
+  validates :image_url, allow_blank: true, format: {
+  	with:    %r{\.(gif|jpg|png)$}i,
+  	message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
