90b6780f6e17a1fba404f97cb16dbdbe5f217297
Gary Lai <garylai1990@gmail.com>
2012-11-08 09:23:04 +0800

    Smarter Cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,6 @@ class ApplicationController < ActionController::Base
       cart
     end
 
-    def current_cart 
-      Cart.find(session[:cart_id])
-    rescue ActiveRecord::RecordNotFound
-      cart = Cart.create
-      session[:cart_id] = cart.id
-      cart
-    end
-
-
 		def current_user
 		  @current_user ||= User.find(session[:user_id]) if session[:user_id]
 		end
