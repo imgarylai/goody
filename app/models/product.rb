30e8f9a01fe515810917d8feeb94e93aebee3888
Gary Lai <garylai1990@gmail.com>
2012-11-20 17:24:58 +0800

    nothing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,26 +1,26 @@
 class Product < ActiveRecord::Base
-	
-	has_many :line_items
-	has_many :orders, :through => :line_items
-  
+
+  has_many :line_items
+  has_many :orders, :through => :line_items
+
   before_destroy :ensure_not_referenced_by_any_line_item
 
   attr_accessible :description, :image_url, :price, :quantity, :title, :date
   validates :description, :image_url, :price, :quantity, :title, :date, presence: true
   validates :price, :quantity, numericality: {greater_than_or_equal_to: 0}
   validates :image_url, allow_blank: true, format: {
-  	with:    %r{\.(gif|jpg|png)$}i,
-  	message: 'must be a URL for GIF, JPG or PNG image.'
+    with:    %r{\.(gif|jpg|png)$}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
   }
 
   private
-    # ensure that there are no line items referencing this product
-		def ensure_not_referenced_by_any_line_item 
-			if line_items.empty?
-				return true 
-			else
-				errors.add(:base, 'Line Items present')
-				return false 
-			end
-		end
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item 
+    if line_items.empty?
+      return true 
+    else
+      errors.add(:base, 'Line Items present')
+      return false 
+    end
+  end
 end
